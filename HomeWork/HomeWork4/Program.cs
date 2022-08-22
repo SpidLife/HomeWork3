37b9782b90ffc9aa9d528f7a93d0262c0bf2be1c
SpidLife <dahm01@mail.ru>
2022-08-23 00:37:21 +0500

    HomeWork4 Done

--- HomeWork/HomeWork4/Program.cs
+++ HomeWork/HomeWork4/Program.cs
@@ -19,7 +19,7 @@ return exp;
 }
 */
 // Задача 27
-
+/*
  Console.WriteLine("Введите число ");
  int number = int.Parse(Console.ReadLine());
  int result = GetSumOfFigures(number);
@@ -35,7 +35,45 @@ int GetSumOfFigures(int num)
     }
     return summ;
 }
-
+*/
 // Задача 29
 
+ Console.WriteLine("Введите 1-е число");
+ int n1 = int.Parse(Console.ReadLine());
 
+ Console.WriteLine("Введите 2-е число");
+ int n2 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 3-е число");
+ int n3 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 4-е число");
+ int n4 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 5-е число");
+ int n5 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 6-е число");
+ int n6 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 7-е число");
+ int n7 = int.Parse(Console.ReadLine());
+
+ Console.WriteLine("Введите 8-е число");
+ int n8 = int.Parse(Console.ReadLine());
+
+
+ int[] array = { n1, n2, n3, n4, n5, n6, n7, n8 };
+
+
+ void PrintArray(int[] arr)
+ {
+    
+     for (int i = 0; i < arr.Length; i++)
+     {
+         System.Console.Write($"{arr[i]} ");
+     }
+    
+ }
+
+ PrintArray(array);
