5c82602a6d2ffccf7a4c4b250477d5daaa1ecf13
SpidLife <dahm01@mail.ru>
2022-08-30 21:11:59 +0500

    HomeWork6 Done

---
+++ HomeWork/HomeWork6/Program.cs
@@ -0,0 +1,94 @@
+﻿//Задача 41
+/*
+Console.Write("Введите количество чисел ");
+ 
+ int number = Convert.ToInt32(Console.ReadLine());
+ 
+ int[] Array = FillManualArray(number);
+
+int[] FillManualArray(int number)
+
+{
+    int[] num = new int[number];
+    System.Console.WriteLine($"Введите номер для ручного заполнения массива {number} раз");
+    for (int i = 0; i < number; i++)
+    {
+
+        num[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int CheckCountOfPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+ int countPositiveNumbers = CheckCountOfPositiveNumbers(Array);
+ 
+ System.Console.WriteLine();
+ 
+ System.Console.Write($"{countPositiveNumbers} это положительное число чисел в массиве ");
+ 
+ PrintArray(Array);
+ */
+//Задача 43
+
+double[,] coeff = new double[2, 2];
+double[] crossPoint = new double[2];
+
+void InputCoefficients(){
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-го уравнения (y = k * x + b):\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] Decision(double[,] coeff)
+{
+  crossPoint[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  crossPoint[1] = crossPoint[0] * coeff[0,0] + coeff[0,1];
+  return crossPoint;
+}
+
+void OutputResponse(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"\nПрямые параллельны");
+  }
+  else 
+  {
+    Decision(coeff);
+    Console.Write($"\nТочка пересечения прямых: ({crossPoint[0]}, {crossPoint[1]})");
+  }
+}
+
+InputCoefficients();
+OutputResponse(coeff);
